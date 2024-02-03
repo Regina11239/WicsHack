0ae9e503e1e9f1036645c85ec7c143dc0bc58f4b
Heyu Zhou <regina.zhou@utexas.edu>
2024-02-03 14:59:02 -0600

    start

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/progress')
+def about():
+    return render_template('progress.html')
+
+@app.route('/forum')
+def contact():
+    return render_template('forum.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
